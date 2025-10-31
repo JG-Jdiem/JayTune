f78faf865c7d0c16dda75722529911f1bf2d1083
Jdiem <154918582+JG-Jdiem@users.noreply.github.com>
2025-10-31 09:18:34 +0100

    Add files via upload

--- compose/persist/src/main/kotlin/app/vitune/compose/persist/Persist.kt
+++ compose/persist/src/main/kotlin/app/vitune/compose/persist/Persist.kt
@@ -1,4 +1,4 @@
-package app.vitune.compose.persist
+package app.jaytune.compose.persist
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
--- compose/persist/src/main/kotlin/app/vitune/compose/persist/PersistMap.kt
+++ compose/persist/src/main/kotlin/app/vitune/compose/persist/PersistMap.kt
@@ -1,4 +1,4 @@
-package app.vitune.compose.persist
+package app.jaytune.compose.persist
 
 import android.util.Log
 import androidx.compose.runtime.MutableState
